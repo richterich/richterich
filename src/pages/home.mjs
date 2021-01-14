5fce002e22d8a133a3aa1e3c3df733bc215cc62a
Alexander Richterich <richterich@yahoo.com>
2021-01-14 22:51:53 +0200

    Switch to front-end code

--- src/pages/home.mjs
+++
@@ -1,9 +0,0 @@
-import fs  from 'fs';
-
-function renderHomePage(response) {
-    let json = fs.readFileSync('./templates/index.json');
-    let homePage = JSON.parse(json);
-    response.render('index', homePage);
-}
-
-export { renderHomePage };
